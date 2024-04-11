b1a1058d02777a9bf343a0de22f810261dce8fa4
SurekaaBalasubramaniyam <surekaabalasubramaniyam@gmail.com>
2024-04-11 17:16:59 +0530

    Hibernate

---
+++ src/main/java/com/Rest/API/service/StudentService.java
@@ -0,0 +1,56 @@
+package com.Rest.API.service;
+
+
+import com.Rest.API.dao.StudentRespository;
+import com.Rest.API.model.Student;
+import org.hibernate.annotations.NotFoundAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRespository studentRespository;
+    public List<Student> getAllStudents(){
+
+        return studentRespository.findAll();
+    }
+
+    public Student getStudent(Long id){
+         Optional<Student> maybeStudent = studentRespository.findById(id);
+         return maybeStudent.orElse(null);
+    }
+
+    public Student createStudent(Student student){
+        Student student1 = studentRespository.save(student);
+        return student1;
+    }
+
+    public Student updateStudent(Long id, Student student){
+
+        boolean studentExist = studentRespository.existsById(id);
+
+        if(studentExist){
+            return studentRespository.save(student);
+        }
+
+       return null;
+
+    }
+
+    public void deleteStudent(Long id){
+
+        boolean studentExist = studentRespository.existsById(id);
+
+        if(studentExist){
+            studentRespository.deleteById(id);
+        }
+
+    }
+}
